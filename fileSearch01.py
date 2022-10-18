b3fff9562d1114863c239d6def8699c11ddf4b33
Ashley <abhayburn1@my.scccd.edu>
2022-10-18 14:50:36 -0700

    Not complete yet, but making progress. Update 1.

--- fileSearch01.py
+++ fileSearch01.py
@@ -36,24 +36,42 @@
 #   7) Use a Python dictionary to count the number of messages from each sender
 #   8) Demonstrate tuples in a dictionary
 
-# Open the file.
 file_name = "mbox-short.txt"
 my_file_handle = open(file_name)
 
 # set accumulator to 0
-# TODO: What is an accumulator and how are they used?
+# An accumulator "accumulates" the desired amount to the variables initial value. Here, the initial value is 0, and
+# later we will be adding, subtracting, multiplying, dividing, totaling, etc. some amount to adjust this value based
+# on our function's directions.
 count = 0
-# Read the file line by line.
-# TODO: Where did my_file_handle come from? How was it created? What is it? What is my_line?
+
+# The my_file_handle came from the variable we used above to access our external file called mbox-short.txt
+# (which is the relative file path). It is easier to type that variable name than to re-type the file name every time we
+# need to access it for information. You can technically make the variable whatever you want it to be, but it should be
+# recognizable in case you have to share your code or work with others on it. The my_file_handle is the file handler
+# telling this program that we want to open the file indicated inside the file_name variable. The my_line is part of
+# the for-loop which is in place to test the file for certain conditions and increase the counter if conditions meet the
+# criteria set within the loop.
 for my_line in my_file_handle:
-    # TODO: What does [0:5] mean? What is this decision control structure checking?
-    if (my_line[0:5] == "From "):
-        # TODO: What are the elements of a list? What does split() do?
+    # The [0:5] refers to the index we are searching for within the info represented by the my_line variable. In this
+    # case, we are mapping the first index (0) through the fourth (5). This says that if those indexes of the line
+    # are equal to the string "From " then the condition of the loop is true and the program should print the info
+    # directed below that if statement.
+    # In this situation the decision control structure is checking to see if the specified indexes match the string
+    # "From "
+    if my_line[0:5] == "From ":
+        # The elements of a list refer to each object in a list. They could be individual letters, numbers, words, or
+        # sentences. Whatever it is you are listing. They get separated by commas, whatever they are. You can sort
+        # through them using their index within the list, which is called index-based traversal.
+        # The split() method splits the strings for a list. You pick the separator, in this case it is the "@" symbol.
         my_list_of_words = my_line.split()
         print("dir(list):")
-        # TODO: What is x[1] here?
+        # Here, x[1] refers to the full email address in the list which will be printed due to the usage of the print
+        # command.
         print(my_list_of_words[1])
-        # TODO: What is happening here?
+        # Next we are re-printing the email address again, but instead of printing it like before, we are splitting the
+        # email address in two at the "@" symbol. The result shows with the first half and second half separated by a
+        # comma.
         two_parts = my_list_of_words[1].split('@')
         print(two_parts)
         prefix = two_parts[0]
